924a66c2539ace0a586d1b2b22501ea15681083a
Mate Timar <timarmatealpine@gmail.com>
2017-11-10 22:51:41 +0100

    plot, import, class

---
+++ point.py
@@ -0,0 +1,6 @@
+''' point class ny mate and csincsi '''
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
--- pyground.py
+++ pyground.py
@@ -1,7 +1,18 @@
-print('Hello World!')
+''' python playground Csincsi <3 Mate '''
 
-v1 = input('Choose a number!')
-v2 = input('Choose a number!')
+import numpy as np
+import matplotlib.pyplot as plt
+from point import *
 
+p = Point(1, 2)
 
-print(float(v1) + float(v2))
+print(p.x, p.y)
+
+plt.figure(1)
+t = np.arange(0., 5., 0.2)
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+
+plt.figure(2)
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
